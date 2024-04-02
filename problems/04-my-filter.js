2ba1dcbd5264193921bdda17b8f6beacd0ec4f1a
Alex Garbarino <alexiothebrave@gmail.com>
2024-04-01 22:39:40 -0500

    04 solved / DRV: Alex / NAV: Damon

--- problems/04-my-filter.js
+++ problems/04-my-filter.js
@@ -20,7 +20,14 @@ console.log(result2);      // ['choose', 'words', 'only']
 *******************************************************************************/
 
 function myFilter(array, cb) {
-    // Your code here 
+   let newArr = [];
+   for (let i = 0; i < array.length; i++) {
+    let el = array[i];
+    if (cb(el)) {
+        newArr.push(el)
+    }
+   }
+   return newArr;
 }
 
 /*****************DO NOT MODIFY ANYTHING UNDER THIS  LINE**********************/
