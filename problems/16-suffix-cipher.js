3eecf19836c27fcfa6f95382aee4e3deb4d161fd
Alex Garbarino <alexiothebrave@gmail.com>
2024-04-02 21:54:52 -0500

    16 solved / DRV: Alex / NAV: Damon

--- problems/16-suffix-cipher.js
+++ problems/16-suffix-cipher.js
@@ -30,11 +30,51 @@ let cipher2 = {
 console.log(suffixCipher('incremental progress is very instrumental', cipher2));
 // INCREMENTAL progressth isth very INSTRUMENTAL
 *******************************************************************************/
+// function helper(word, cipher) {
+//   let keys = Object.keys(cipher);
+// }
 
 function suffixCipher(sentence, cipher) {
-  // Your code here 
+  let keys = Object.keys(cipher);
+  let values = Object.values(cipher);
+  let arr = [];
+  let words = sentence.split(' ');
+  // go through array and compare word endings with suffixes
+  for (let i = 0; i < words.length; i++) {
+    let el = words[i];
+    for (let key in cipher) {
+      if (el.endsWith(key)) {
+        words[i] = cipher[key](el)
+
+      //  arr.push(cipher[key](el))
+      // } if (!el.endsWith(key)){
+      //   arr.push(el);
+      }
+
+    }
+  }
+  return words.join(' ');
 }
 
+let cipher1 = {
+  ly: function(word) {
+      return word.slice(0, -1) + 'ee';
+  },
+  ize: function(word) {
+      return word + 'r';
+  }
+};
+console.log(suffixCipher('quietly and gently visualize', cipher1));
+// quietlee and gentlee visualizer
+
+let cipher2 = {
+  tal: function(word) {
+      return word.toUpperCase();
+  },
+  s: function(word) {
+      return word + 'th';
+  }
+};
 /*****************DO NOT MODIFY ANYTHING UNDER THIS  LINE**********************/
 try {
   module.exports = suffixCipher;
