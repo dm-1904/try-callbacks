552dc5b7434aafcd33a6b0961ae1087ed252f824
damonryon <damon.ryon@gmail.com>
2024-04-02 19:18:54 -0700

    15 completed

--- problems/15-sentence-mapper.js
+++ problems/15-sentence-mapper.js
@@ -26,7 +26,13 @@ console.log(result2); // 'ths s prtty cl rght'
 *******************************************************************************/
 
 let sentenceMapper = function (sentence, cb) {
-  // Your code here 
+  let sentenceArr = sentence.split(' ')
+  let arr = []
+  for(let i = 0; i < sentenceArr.length; i++){
+    let el = sentenceArr[i]
+    arr.push(cb(el))
+  }
+  return arr.join(' ')
 };
 
 /*****************DO NOT MODIFY ANYTHING UNDER THIS  LINE**********************/
