27a833e790dab052866e1a1ed9660f15c378f294
Alex Garbarino <alexiothebrave@gmail.com>
2024-04-02 22:16:55 -0500

    20 solved / DRV: Alex / NAV: Damon

--- problems/20-none.js
+++ problems/20-none.js
@@ -29,7 +29,17 @@ console.log(result4);   // false
 *******************************************************************************/
 
 function none(array, cb) {
-  // Your code here 
+  let count = 0;
+  for (let i = 0; i < array.length; i++) {
+    let el = array[i];
+    if (cb(el)) {
+      count++
+    }
+  }
+  if (count === 0) {
+  return true
+  }
+  return false
 }
 
 /*****************DO NOT MODIFY ANYTHING UNDER THIS  LINE**********************/
